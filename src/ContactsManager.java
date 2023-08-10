5f58c284da3bb1c99a77cb474d04dce3f54092d3
Audrey Tan <audrey.wanmun.tan@gmail.com>
2023-08-10 11:10:05 -0500

    created contacts file

--- src/ContactsManager.java
+++ src/ContactsManager.java
@@ -1,5 +1,13 @@
-public class ContactsManager {
-    public static void main(String[] args) {
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
+public class ContactsManager {
+public static Path contactsList = Paths.get("contacts.txt");
+
+
+
+    public static void main(String[] args) {
+//        System.out.println(contactsList);
     }
+
 }
